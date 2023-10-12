fc44702100f526546fc02c3e04d3fdba51939d00
arturo317 <112517092+arturo317@users.noreply.github.com>
2023-10-12 16:31:52 -0600

    proyecto  version 1.0
    primera modificacion del proyecto

---
+++ config.py
@@ -0,0 +1,21 @@
+# config.py
+
+# Clave secreta para la aplicación Flask
+SECRET_KEY = 'tu_clave_secreta'
+
+# Configuración de la base de datos MySQL
+DATABASE_CONFIG = {
+    
+    'host': 'containers-us-west-92.railway.app',
+    'user': 'root',
+    'password': 'tZpcPt65X7uH432eGm1Q',
+    'database': 'railway',
+    'port': 7799  # Puerto MySQL
+    
+    #'host': 'localhost',              # Cambia a la dirección de tu servidor MySQL
+    #'user': 'root',                  # Cambia al nombre de usuario de MySQL
+    #'password': '123456',     # Cambia a tu contraseña de MySQL
+    #'database': 'ProyectoWeb'      # Cambia al nombre de tu base de datos
+}
+
+
