8937f03552b54b21f5613ad647dc7d905d24a39d
JaimeOmar1904 <jaimemarcial@comunidad.unam.mx>
2023-10-24 12:47:02 -0600

    Modificaciones de login

--- Pagina_Juegos/Login/README.md
+++
@@ -1,4 +0,0 @@
-# Hacker Login Form
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/Apex-Coder/pen/mdQJreW](https://codepen.io/Apex-Coder/pen/mdQJreW).
-
--- Pagina_Juegos/Login_new/config.py
+++ Pagina_Juegos/Login_new/config.py
@@ -5,10 +5,11 @@ SECRET_KEY = 'tu_clave_secreta'
 
 # Configuración de la base de datos MySQL
 DATABASE_CONFIG = {
-    'host': 'db4free.net',              # Cambia a la dirección de tu servidor MySQL
-    'user': 'davo63',                  # Cambia al nombre de usuario de MySQL
-    'password': 'david123',     # Cambia a tu contraseña de MySQL
-    'database': 'aplicacionweb'      # Cambia al nombre de tu base de datos
+    'host': 'containers-us-west-92.railway.app',              # Cambia a la dirección de tu servidor MySQL
+    'user': 'root',                  # Cambia al nombre de usuario de MySQL
+    'password': 'tZpcPt65X7uH432eGm1Q',     # Cambia a tu contraseña de MySQL
+    'database': 'railway',      # Cambia al nombre de tu base de datos
+    'port': '7799'
 }
 
 
